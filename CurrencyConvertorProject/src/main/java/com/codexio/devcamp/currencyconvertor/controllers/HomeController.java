a8cf106bc20a07418953f17121cb208157b1a4e9
mario <axereliat@gmail.com>
2019-06-06 16:31:45 +0300

    Basic stuff

---
+++ CurrencyConvertorProject/src/main/java/com/codexio/devcamp/currencyconvertor/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.codexio.devcamp.currencyconvertor.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home/index";
+    }
+}
