1a974095ed3518097c5ee1fb4df3eb270f624b5d
sa6o sa6o <aleksandarboev95@gmail.com>
2019-06-07 18:47:30 +0300

    Binding model validations, some unit tests, serviceimpl

--- CurrencyConvertorProject/src/main/java/com/codexio/devcamp/currencyconvertor/app/services/CurrencyServiceImpl.java
+++ CurrencyConvertorProject/src/main/java/com/codexio/devcamp/currencyconvertor/app/services/CurrencyServiceImpl.java
@@ -1,11 +1,17 @@
 package com.codexio.devcamp.currencyconvertor.app.services;
 
+import com.codexio.devcamp.currencyconvertor.app.domain.entities.Currency;
 import com.codexio.devcamp.currencyconvertor.app.repository.CurrencyRepository;
 import com.codexio.devcamp.currencyconvertor.app.utils.CurrencyScrape;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class CurrencyServiceImpl implements CurrencyService {
@@ -17,10 +23,24 @@ public class CurrencyServiceImpl implements CurrencyService {
         this.currencyScrape = currencyScrape;
     }
 
-
     @Override
     @Scheduled(cron = "* * * * * ?")
     public void seedCurrencies() throws IOException {
-        this.currencyScrape.getCurrencyNameEuroRate();
+        try {
+            Map<String, String> rawCurrencies = this.currencyScrape.getCurrencyNameEuroRate();
+            List<Currency> currencyList = new ArrayList<>();
+            for (Map.Entry<String, String> stringStringEntry : rawCurrencies.entrySet()) {
+                Currency currency = new Currency();
+                currency.setName(stringStringEntry.getKey());
+                currency.setEuroRate(new BigDecimal(stringStringEntry.getValue()));
+
+                currencyList.add(currency);
+            }
+
+            this.currencyRepository.saveAll(currencyList);
+        } catch (Exception e) {
+            //TODO
+        }
+
     }
 }
