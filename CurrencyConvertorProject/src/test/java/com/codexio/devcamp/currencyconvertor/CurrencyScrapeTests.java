15b69eab59dad696fd26a5e27b629ca207a026be
sa6o sa6o <aleksandarboev95@gmail.com>
2019-06-13 17:33:24 +0300

    Minor changes

--- CurrencyConvertorProject/src/test/java/com/codexio/devcamp/currencyconvertor/CurrencyScrapeTests.java
+++ CurrencyConvertorProject/src/test/java/com/codexio/devcamp/currencyconvertor/CurrencyScrapeTests.java
@@ -2,6 +2,9 @@ package com.codexio.devcamp.currencyconvertor;
 
 import com.codexio.devcamp.currencyconvertor.app.domain.models.SeedCurrencyBindingModel;
 import com.codexio.devcamp.currencyconvertor.app.utils.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +18,8 @@ import javax.validation.Validator;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
@@ -23,6 +28,7 @@ public class CurrencyScrapeTests {
     private CurrencyReader secondaryCurrencyScrape;
     private ValidatorUtil validator;
     private Validator myValidator;
+    private HistoryCurrencyScrape historyCurrencyScrape;
 
     @Before
     public void init() {
@@ -30,6 +36,7 @@ public class CurrencyScrapeTests {
         this.secondaryCurrencyScrape = new SecondaryCurrencyScrape();
         this.validator = new ValidatorUtilImpl();
         this.myValidator = Validation.buildDefaultValidatorFactory().getValidator();
+        this.historyCurrencyScrape = new HistoryCurrencyScrape();
     }
 
     @Test
@@ -38,7 +45,9 @@ public class CurrencyScrapeTests {
 
         Assert.assertNotNull(scrapingResult);
         Assert.assertTrue(!scrapingResult.isEmpty());
-        scrapingResult.forEach(e -> Assert.assertTrue(this.buildErrorsString(this.validator.violations(e)), this.validator.isValid(e)));
+
+        String allErrors = this.buildAllErrorsString(scrapingResult);
+        Assert.assertTrue(allErrors, allErrors.equals(""));
     }
 
     @Test
@@ -47,13 +56,27 @@ public class CurrencyScrapeTests {
 
         Assert.assertNotNull(scrapingResult);
         Assert.assertTrue(!scrapingResult.isEmpty());
-        scrapingResult.forEach(e -> Assert.assertTrue(this.buildErrorsString(this.validator.violations(e)), this.validator.isValid(e)));
+
+        String allErrors = this.buildAllErrorsString(scrapingResult);
+        Assert.assertTrue(allErrors, allErrors.equals(""));
+    }
+
+    private String buildAllErrorsString(List<SeedCurrencyBindingModel> seedCurrencyBindingModels) {
+        StringBuilder sb = new StringBuilder();
+        for (SeedCurrencyBindingModel currentBindingModel : seedCurrencyBindingModels) {
+            Set<ConstraintViolation<Object>> violations = this.validator.violations(currentBindingModel);
+
+            if (violations.isEmpty())
+                continue;
+
+            sb.append(this.buildErrorsString(violations)).append(System.lineSeparator());
+        }
+
+        return sb.toString().trim();
     }
 
     /**
-     *
-     * @param violations
-     * @return String in format "<problematic value> --> <raised exception message>"
+     * @return String in format <br>"problematic value --> raised exception message"
      */
     private String buildErrorsString(Set<ConstraintViolation<Object>> violations) {
         StringBuilder sb = new StringBuilder();
